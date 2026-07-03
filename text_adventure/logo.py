24dc616999f43778b864c367eab77fafe584da0d
lordfunion <lordfunion@gmail.com>
2026-07-03 19:02:58 -0400

    Update ASCII logo for Realmbound

--- text_adventure/logo.py
+++ text_adventure/logo.py
@@ -9,23 +9,18 @@ def _print_text_logo():
     print(
         Fore.LIGHTYELLOW_EX
         + r"""
-    ___       __                 __
-   /   | ____/ /   _____  ____  / /___  __________
-  / /| |/ __  / | / / _ \/ __ \/ __/ / / / ___/ _ \
- / ___ / /_/ /| |/ /  __/ / / / /_/ /_/ / /  /  __/
-/_/  |_\__,_/ |___/\___/_/ /_/\__/\__,_/_/   \___/
-          ______
-         / ____/___ _____ ___  ___
-        / / __/ __ `/ __ `__ \/ _ \
-       / /_/ / /_/ / / / / / /  __/
-       \____/\__,_/_/ /_/ /_/\___/
+ ____            _           _                           _
+|  _ \ ___  __ _| |_ __ ___ | |__   ___  _   _ _ __   __| |
+| |_) / _ \/ _` | | '_ ` _ \| '_ \ / _ \| | | | '_ \ / _` |
+|  _ <  __/ (_| | | | | | | | |_) | (_) | |_| | | | | (_| |
+|_| \_\___|\__,_|_|_| |_| |_|_.__/ \___/ \__,_|_| |_|\__,_|
 """
         + Style.RESET_ALL
     )
 
 
 def show_startup_logo():
-    """Show the adventure logo before the main menu or loaded save starts."""
+    """Show the Realmbound logo before the main menu or loaded save starts."""
     if os.getenv("TEXT_ADVENTURE_HIDE_LOGO"):
         return
 
